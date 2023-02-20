0c285858a2fd2302a0a1b67e11952204ecb54533
Lucas dos Santos <sl.solucas@gmail.com>
2023-02-19 21:34:19 -0300

    Update script.js

--- Hora do Dia/script.js
+++ Hora do Dia/script.js
@@ -4,8 +4,7 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 15
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 6 && hora <= 12){
         saudacao.innerHTML = 'Bom Dia !!'
